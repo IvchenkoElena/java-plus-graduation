3d2f30220bd063eedc16b9c020f65d20a394fd47
IvchenkoEV <pesok72@mail.ru>
2025-05-31 11:45:26 +0500

    реализован модуль analyzer

---
+++ stats/analyzer/src/main/java/ru/practicum/repository/UserActionRepository.java
@@ -0,0 +1,19 @@
+package ru.practicum.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.practicum.model.UserAction;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Repository
+public interface UserActionRepository extends JpaRepository<UserAction, Long> {
+    Optional<UserAction> findByUserIdAndEventId(Long userId, Long eventId);
+
+    Set<UserAction> findByUserId(Long userId);
+
+    List<UserAction> findByEventIdIsIn(List<Long> eventIds);
+
+}
